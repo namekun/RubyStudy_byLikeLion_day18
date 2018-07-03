3174731f3726b381f474baea290677a8808a353b
NamHyuk KIM <happykimnh@icloud.com>
2018-07-03 08:26:04 +0000

    20180703JS

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,52 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url(*args)
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # Process files as they are uploaded:
+  # process scale: [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
+
+  # Create different versions of your uploaded files:
+  version :thumb_fit do
+    process resize_to_fit: [240, 180] # 해당 사이즈에 맞게 잘라준다.
+  end
+  
+  version :thumb_fill do
+    process resize_to_fill: [240, 180] # 해당 사이즈에 맞게 이미지를 맞춘다.
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  # def extension_whitelist # 아래의 확장자가 아닐시 업로드가 불가하다.
+  #   %w(jpg jpeg gif png)
+  # end
+
+  # # Override the filename of the uploaded files:
+  # # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # def filename # 파일의 이름을 맘대로 저장할 수 있다.
+  #   "something.jpg" if original_filename
+  # end
+  
+end
